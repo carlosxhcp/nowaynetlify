82a57694e0c32fd84fc4e82222b1069093751af7
carlosxhcp <carlosxhcp@gmail.com>
2023-11-27 12:40:26 -0300

    mudanças gerais em arquivos de fotos dos slides

--- js/buttons.js
+++ js/buttons.js
@@ -11,3 +11,26 @@ buttons1.forEach(button => {
     })
 });
 
+
+// SCRIPT DE MUDAR O LINK DO BOTAO
+function changeLinkHrefM() {
+    const link = document.getElementById('link');
+
+    link.setAttribute('href', 'https://bobbyhadz.com');
+
+    return false;
+  }
+  function changeLinkHrefG() {
+    const link = document.getElementById('link');
+
+    link.setAttribute('href', 'https://www.youtube.com/watch?v=0a99Pe1qZ_0');
+
+    return false;
+  }
+  function changeLinkHrefGG() {
+    const link = document.getElementById('link');
+
+    link.setAttribute('href', 'https://moewalls.com/category/abstract/');
+
+    return false;
+  }
