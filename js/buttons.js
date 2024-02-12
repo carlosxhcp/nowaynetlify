6bed117006213397b474cc02a6a0a12671f4e824
carlosxhcp <carlosxhcp@gmail.com>
2024-02-11 21:17:07 -0300

    aaa

--- js/buttons.js
+++ js/buttons.js
@@ -16,21 +16,21 @@ buttons1.forEach(button => {
 function changeLinkHrefM() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://bobbyhadz.com');
+    link.setAttribute('href', 'https://nowaysp.pay.yampi.com.br/r/9EO0MRQXUA');
 
     return false;
   }
   function changeLinkHrefG() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://www.youtube.com/watch?v=0a99Pe1qZ_0');
+    link.setAttribute('href', 'https://nowaysp.pay.yampi.com.br/r/9A1JEOUUA0');
 
     return false;
   }
   function changeLinkHrefGG() {
     const link = document.getElementById('link');
 
-    link.setAttribute('href', 'https://moewalls.com/category/abstract/');
+    link.setAttribute('href', 'https://nowaysp.pay.yampi.com.br/r/AL7AASAA8Y');
 
     return false;
   }
